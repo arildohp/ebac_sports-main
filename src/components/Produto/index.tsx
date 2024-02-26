85e70edc165bde2ef6d232cb0dea948d29f14f12
arildo <arildohp@gmail.com>
2024-02-26 11:39:17 -0300

    atualizando projeto

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -14,33 +14,24 @@ export const paraReal = (valor: number) =>
     valor
   )
 
-const ProdutoComponent = ({ produto, favoritar, estaNosFavoritos }: Props) => {
-  const Produto = ({ produto }: Props) => {
-    const dispatch = useDispatch()
+const Produto = ({ produto }: Props) => {
+  const dispatch = useDispatch()
 
-    return (
-      <S.Produto>
-        <S.Capa>
-          <img src={produto.imagem} alt={produto.nome} />
-        </S.Capa>
-        <S.Titulo>{produto.nome}</S.Titulo>
-        <S.Prices>
-          <strong>{paraReal(produto.preco)}</strong>
-        </S.Prices>
-        <S.BtnComprar onClick={() => favoritar(produto)} type="button">
-          {estaNosFavoritos
-            ? '- Remover dos favoritos'
-            : '+ Adicionar aos favoritos'}
-        </S.BtnComprar>
-        <S.BtnComprar
-          onClick={() => dispatch(adicionar(produto))}
-          type="button"
-        >
-          Adicionar ao carrinho
-        </S.BtnComprar>
-      </S.Produto>
-    )
-  }
+  return (
+    <S.Produto>
+      <S.Capa>
+        <img src={produto.imagem} alt={produto.nome} />
+      </S.Capa>
+      <S.Titulo>{produto.nome}</S.Titulo>
+      <S.Prices>
+        <strong>{paraReal(produto.preco)}</strong>
+      </S.Prices>
+
+      <S.BtnComprar onClick={() => dispatch(adicionar(produto))} type="button">
+        Adicionar ao carrinho
+      </S.BtnComprar>
+    </S.Produto>
+  )
 }
 
-export default ProdutoComponent
+export default Produto
